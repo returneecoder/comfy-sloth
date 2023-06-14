2cdcce2b56b24db1347f81df6403b0516ca54814
returneecoder <akhtar.amna81@gmail.com>
2023-06-14 21:13:58 +0400

    latest commit

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -6,22 +6,23 @@ exports.handler = async function (event, context) {
    
     
   if (event.body) {
+    
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
-
+    
     const calculateOrderAmount = () => {
       return shipping_fee + total_amount;
     };
     try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(),
-        currency: "usd",
-      });
-      
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-      };
-    } catch (error) {
+        // Create a PaymentIntent with the order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: calculateOrderAmount(),
+          currency: 'usd',
+        })
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+        }
+      } catch (error) {
       return {
         statusCode: 500,
         body: JSON.stringify({ msg: error.message }),
